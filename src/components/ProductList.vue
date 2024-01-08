1975f8316000ef5f095e45e2506fc7bcae060e6a
valdir-amaral <valdir.amaral58@gmail.com>
2024-01-08 15:44:14 -0300

    feat: add "api"

---
+++ src/components/ProductList.vue
@@ -0,0 +1,37 @@
+<template>
+    <section class="products-container">
+        <div v-for="product in products" :key="product.id">
+            <img v-if="product.photos" :src="product.photos[0].src" :alt="product.photos[0].title">
+            <p class="price">{{ product.price }}</p>
+            <h2 class="title">{{ product.name }}</h2>
+            <p>{{ product.description }}</p>
+        </div>
+    </section>
+</template>
+
+<script>
+import { api } from '@/services.js';
+
+
+export default {
+    data() {
+        return {
+            products: null
+        }
+    },
+    methods: {
+        getProducts() {
+            api.get("/products").then(res => {
+                this.products = res.data;
+            })
+        }
+    },
+    created() {
+        this.getProducts();
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,30 @@
 <template>
-  <div>
-
-  </div>
+  <section>
+    <h1>🛒&nbsp; Compre ou Venda &nbsp; 📢</h1>
+    <ProductSearch />
+    <ProductList />
+  </section>
     
 </template>
 
 <script>
+import ProductSearch from '@/components/ProductSearch.vue';
+import ProductList from '@/components/ProductList.vue';
+
 export default {
   name: 'HomeView',
+  components: {
+    ProductSearch,
+    ProductList
+  }
 }
 </script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #87f;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
