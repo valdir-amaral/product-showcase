b5cc04556928de679d485670511e4c94cf927609
valdir-amaral <valdir.amaral58@gmail.com>
2024-01-08 17:01:02 -0300

    style: search input and global styles

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,11 +1,23 @@
 <template>
     <section class="products-container">
-        <div v-for="product in products" :key="product.id">
-            <img v-if="product.photos" :src="product.photos[0].src" :alt="product.photos[0].title">
-            <p class="price">{{ product.price }}</p>
-            <h2 class="title">{{ product.name }}</h2>
-            <p>{{ product.description }}</p>
+        <div v-if="products && products.length" class="products">
+            <div class="product" v-for="product in products" :key="product.id">
+                <RouterLink to="/">
+                    <img v-if="product.photos" :src="product.photos[0].src" :alt="product.photos[0].title">
+                    <p class="price">{{ product.price }}</p>
+                    <h2 class="title">{{ product.name }}</h2>
+                    <p>{{ product.description }}</p>
+                </RouterLink>
+                
+            </div>
         </div>
+        <div v-else-if="products && !products.length">
+            <p class="no-results">Busca sem resultados :/ Tente buscar outro termo.</p>
+        </div>
+        <div v-else>
+            <p>Carregando...</p>
+        </div>
+        
     </section>
 </template>
 
@@ -46,5 +58,45 @@ export default {
 </script>
 
 <style scoped>
+.products-container {
+    max-width: 1000px;
+    margin: 0 auto;
+}
+.products {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 30px;
+    margin: 30px;
+}
 
+.product {
+    box-shadow: 0 4px 8px rgba(30,60,90,.1);
+    padding: 10px;
+    background: white;
+    border-radius: 4px;
+    transition: all .2s;
+}
+
+.product:hover {
+    box-shadow: 0 6px 12px rgba(30,60,90,.2);
+    transform: scale(1.1);
+    position: relative;
+    z-index: 1;
+}
+
+.product img {
+    border-radius: 4px;
+    margin-bottom: 20px;
+}
+
+.title {
+    margin-bottom: 10px;
+}
+.price {
+    color: #e80;
+    font-weight: bold;
+}
+.no-results {
+    text-align: center;
+}
 </style>
--- src/components/ProductSearch.vue
+++ src/components/ProductSearch.vue
@@ -1,6 +1,6 @@
 <template>
     <form>
-        <input name="search" id="search" type="text" v-model="value">
+        <input name="search" id="search" type="text" v-model="value" placeholder="Buscar...">
         <input type="submit" id="send" value="Buscar" @click="searchProducts" @click.prevent>
     </form>
 </template>
@@ -21,5 +21,35 @@ export default {
 </script>
 
 <style scoped>
+form {
+    max-width: 600px;
+    margin: 30px auto 60px auto;
+    position: relative;
+}
 
+#search {
+    width: 100%;
+    padding: 20px;
+    border: none;
+}
+
+#search:focus,
+#search:hover {
+    outline: none;
+    transform: scale(1.1);
+    box-shadow: 0 6px 12px rgba(30,60,90,.2);
+}
+
+#send {
+    width: 62px;
+    height: 62px;
+    text-indent: -150px;
+    background: url('../assets/search.svg') no-repeat center center;
+    border: none;
+    cursor: pointer;
+    position: absolute;
+    right: 0;
+    box-shadow: none;
+    top: 0;
+}
 </style>
