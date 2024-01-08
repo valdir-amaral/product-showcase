82896713965bb65bd8a48c3496f1a2aa29aeb460
valdir-amaral <valdir.amaral58@gmail.com>
2024-01-08 14:21:34 -0300

    feat: add assets and create footer/header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-  </nav>
-  <router-view/>
+  <div id="app">
+    <TheHeader />
+    <main id="main">
+      <router-view/>
+    </main>
+    <TheFooter />
+  </div>
 </template>
 
-<style>
+<script>
+import TheFooter from '@/components/TheFooter.vue'
+import TheHeader from '@/components/TheHeader.vue'
 
+export default {
+  components: {
+    TheHeader,
+    TheFooter
+  }
+}
+</script>
+
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+#main {
+  flex: 1;
+}
 </style>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,24 @@
+<template>
+    <footer>
+        <p>Ranek. Nenhum direito reservado :v</p>
+
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'TheFooter'
+}
+</script>
+
+<style scoped>
+footer {
+    background: #87f;
+    padding: 25px 0;
+    text-align: center;
+    font-size: 1.2rem;
+    color: #fff;
+    position: relative;
+    top: 75px;
+}
+</style>
