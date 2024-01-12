013a3cc5799a31493550a5ef3498ecd5c418991c
valdir-amaral <valdir.amaral58@gmail.com>
2024-01-12 16:58:55 -0300

    feat: add page load component

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-
+import PageLoading from '@/components/PageLoading.vue'
 import "@/assets/global.css"; 
 
-createApp(App).use(store).use(router).mount('#app');
+const app = createApp(App)
+
+app.component(PageLoading)
+app.use(store).use(router).mount('#app');
